2731b31b18c6c252d2288c424ba38b02cd22c11c
Oacte <164973337+Oacte@users.noreply.github.com>
2024-10-22 11:08:26 +0100

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -1,13 +1,12 @@
 /* See LICENSE file for copyright and license details. */
-
+#include <X11/XF86keysym.h>
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "jetbrainsmono nerd font:size=10:antialias=true:autohint=true" };
-static const char dmenufont[]       = "jetbrainsmono nerd font:size=12:antialias=true:autohint=true";
+static const char *fonts[]          = { "Ubuntu Mono:size=13:antialias=true:autohint=true" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -20,7 +19,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6" };
+static const char *tags[] = { "I", "II", "III", "IV ", "V", "VI" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -58,11 +57,17 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
+static const char *termcmd[]  = { "st", NULL };
+static const char *firefox[]  = { "firefox", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+    { 0, XF86XK_AudioRaiseVolume,   spawn,     SHCMD("amixer set Master 5%+ unmute") },
+    { 0, XF86XK_AudioLowerVolume,   spawn,     SHCMD("amixer set Master 5%-") },
+    { 0, XF86XK_AudioMute,          spawn,     SHCMD("amixer set Master toggle") },
+    { ControlMask, XK_a, spawn, SHCMD("thunar") },
+    { 0, XK_Menu,   spawn,  {.v = firefox } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
